7374214bdd1c28b4383626d5265c275b7f65da46
Gabriel <47682862+GabrielApolinario@users.noreply.github.com>
2019-10-01 18:39:52 -0300

    Retirando static dos metodos, Utilizar o Set

--- Triangulo/Triangulo/src/Triangulo.java
+++ Triangulo/Triangulo/src/Triangulo.java
@@ -2,11 +2,10 @@ import javax.swing.JOptionPane;
 
 public class Triangulo {
 
-	private static double lado1;
-	private static double lado2;
-	private static double lado3;
-	private static String nomeTriangulo;
-	private static int resposta;
+	private double lado1;
+	private double lado2;
+	private double lado3;
+	private String nomeTriangulo;
 
 	public double getLado1() {
 		return lado1;
@@ -32,40 +31,38 @@ public class Triangulo {
 		this.lado3 = lado3;
 	}
 
-	public static void nomeTriangulo() {
+	public void nomeTriangulo() {
 		if (lado1 == lado2 && lado2 == lado3) {
-			nomeTriangulo = "Equilátero";
+			nomeTriangulo = "EquilÃ¡tero";
 		} else if (lado1 == lado2 && lado1 != lado3 || lado1 == lado3 && lado2 != lado3) {
-			nomeTriangulo = "Isóceles";
+			nomeTriangulo = "IsÃ³celes";
 		} else if (lado1 != lado2 && lado2 != lado3) {
 			nomeTriangulo = "Escaleno";
 		} else {
-			System.out.println("Este objeto não é um triangulo");
+			System.out.println("Este objeto nÃ£o Ã© um triangulo");
 
 		}
 
 	};
 
-	public static void calculaPerimetro() {
+	public void calculaPerimetro() {
 		if (eTriangulo() == true) {
 			double perimetro = lado1 + lado2 + lado3;
-			System.out.println("O perímetro do triangulo " + nomeTriangulo + " é de " + perimetro);
+			System.out.println("O perÃ­metro do triangulo " + nomeTriangulo + " Ã© de " + perimetro);
 		}
 
 	};
 
-	public static void leDados() {
-		lado1 = Double
-				.parseDouble(JOptionPane.showInputDialog("Insira aqui o valor para o primeiro lado do Triângulo"));
-		lado2 = Double.parseDouble(JOptionPane.showInputDialog("Insira aqui o valor para o segundo lado do Triângulo"));
-		lado3 = Double
-				.parseDouble(JOptionPane.showInputDialog("Insira aqui o valor para o terceiro lado do Triângulo"));
-		Triangulo.eTriangulo();
-		Triangulo.nomeTriangulo();
-		Triangulo.calculaPerimetro();
+	public void leDados() {
+		lado1 = Double.parseDouble(JOptionPane.showInputDialog("Insira aqui o valor para o primeiro lado do TriÃ¢ngulo"));
+		lado2 = Double.parseDouble(JOptionPane.showInputDialog("Insira aqui o valor para o segundo lado do TriÃ¢ngulo"));
+		lado3 = Double.parseDouble(JOptionPane.showInputDialog("Insira aqui o valor para o terceiro lado do TriÃ¢ngulo"));
+		eTriangulo();
+		nomeTriangulo();
+		calculaPerimetro();
 	};
 
-	private static boolean eTriangulo() {
+	private boolean eTriangulo() {
 		if (lado1 + lado2 > lado3) {
 			return true;
 		} else {
@@ -74,25 +71,21 @@ public class Triangulo {
 
 	};
 
-	public static void CriaTriangulo() {
-		 resposta = Integer.parseInt(JOptionPane.showInputDialog(null, "1 – Cadastra Triângulo \n2 – Calcula o Perímetro"
-		  		+ "\n3 – Exibe o nome do triângulo \n4 – Sai do programa", "Menu de Opções", JOptionPane.INFORMATION_MESSAGE));
-		  if(resposta == 1) {
-			  Triangulo.leDados();
-		  } else if(resposta == 2) {
-			  Triangulo.calculaPerimetro();
-		  } else if(resposta == 3) {
-			  Triangulo.eTriangulo();
-		  } else if(resposta == 4) {
-			  System.exit(0);
-		  } else {
-			  System.out.println("Resposta inválida!");
-		  }
-		  while(resposta != 4)
-		  {
-			  resposta = Integer.parseInt(JOptionPane.showInputDialog(null, "1 – Cadastra Triângulo \n2 – Calcula o Perímetro"
-				  		+ "\n3 – Exibe o nome do triângulo \n4 – Sair do programa", "Menu de Opções", JOptionPane.INFORMATION_MESSAGE));
-		  } 
-	}
+	//public static void CriaTriangulo() {
+	//	 resposta = Integer.parseInt(JOptionPane.showInputDialog(null, "1 â€“ Cadastra TriÃ¢ngulo \n2 â€“ Calcula o PerÃ­metro"
+	//	  		+ "\n3 â€“ Exibe o nome do triÃ¢ngulo \n4 â€“ Sai do programa", "Menu de OpÃ§Ãµes", JOptionPane.INFORMATION_MESSAGE));
+	//	  if(resposta == 1) {
+	//		  Triangulo.leDados();
+	//	  } else if(resposta == 2) {
+	//		  Triangulo.calculaPerimetro();
+	//	  } else if(resposta == 3) {
+	//		  Triangulo.eTriangulo();
+	//	  } else if(resposta == 4) {
+	//		  System.exit(0);
+	//	  } else {
+	//		  System.out.println("Resposta invÃ¡lida!");
+	//	  }
+
+	//}
 }
 
